15a4c32e3b715a7400b6a5b512b92033ff804a0b
Laura Vanessa Reyes Vera <lauravanessareyes1@gmail.com>
2026-07-02 09:39:24 -0500

    se ajusto el settings

--- Sena/config/settings.py
+++ Sena/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$u@kk!ne+y*gg=^q)9t+cmi%8@9mo3@@$t&a1ab*_tld%61#k#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
